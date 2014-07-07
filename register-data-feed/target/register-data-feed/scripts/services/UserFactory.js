05571c225b546ac4df977d7296d864db5cc86751
Kunal Limaye <kunallimaye@gmail.com>
2014-07-08 09:52:41 +1000

    removing target folder

--- register-data-feed/target/register-data-feed/scripts/services/UserFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('registerdatafeed').factory('UserResource', function($resource){
-    var resource = $resource('users/users/:UserId',{UserId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
