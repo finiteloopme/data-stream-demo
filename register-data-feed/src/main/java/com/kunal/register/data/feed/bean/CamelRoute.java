4227e1efcd0165f4219fbe0fb99d4b1fbe6292ed
Kunal Limaye <kunallimaye@gmail.com>
2015-10-16 17:59:19 +1100

    Insert user test working

--- register-data-feed/src/main/java/com/kunal/register/data/feed/bean/CamelRoute.java
+++ register-data-feed/src/main/java/com/kunal/register/data/feed/bean/CamelRoute.java
@@ -31,59 +31,9 @@ public class CamelRoute {
 		
 	}
 	
-	public void configure(final String location) throws Exception{
-		camelContext.addRoutes(new RouteBuilder() {
-			
-			@Override
-			public void configure() throws Exception {
-				from("twitter://"
-						+ "streaming/filter?"
-						// query execution type = polling
-						+ "type=polling" + "&"
-						// poll every 10 sec
-						+ "delay=10" + "&"
-						+ "locations=" + location
-						+ "" + "&"
-						+ "consumerKey=HjLbxF1IoN1bXENLDuMPbJsQT" + "&"
-						+ "consumerSecret=62no3XxjQGrgWzASJXkyoW2L0Rs2Ba6Qi1OWFcwilgzOC1rWwW" + "&"
-						+ "accessToken=229302807-NwkYedrDGdZ4CDxvoRcxVtH8klPqfC9Yxt0fD9Fn" + "&"
-						+ "accessTokenSecret=its7D8zTq8GEgCzxHYeCYt4g9HtswwUFBLwrv0bHjngzJ")
-					.log("\n====================\n" +
-						 "\tAuthor: ${body.user.screenName}" + "\n" +
-						 "\t-------" + "\n" +
-						 "\t${body.text}" +"\n" +
-						 "\tTweeted at place: ${body.place}\n" +
-						 "\tTweeted at location: ${body.geoLocation}")
-						 .marshal().json(JsonLibrary.Jackson)
-						 .log("Marshalled to JSON:\n${body}")
-					.setHeader(Exchange.HTTP_METHOD, constant("POST"))
-					.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-					.to(DATA_STREAM_HOST
-							+ "data-stream-cache/"
-							+ "cache/"
-							+ "streaming-data/"
-							+ "new-event/"
-							+ "location/"
-							+ "${body}");
-			}
-		});
-		
-		camelContext.start();
-	}
-	
-//	public void configure(final String username, final String searchCriteria)
 	public void configure(final String username, final String searchCriteria)
 		throws Exception{
 		
-		String[] locations = new String[]{
-				"144.94781,-37.82331;144.98121,-37.80301", //Melbourne
-				"145.0532,-37.8533;145.0866,-37.8329", //Camberwell
-				"151.1988,-33.8683;151.2155,-33.8576", //Sydney
-				"153.0155,-27.4757;153.0322,27.4643" // Brisbane
-				};
-		
-		for(int counter=0 ; counter<locations.length ; counter++){
-		final	String location = locations[counter];
 		camelContext.addRoutes(new RouteBuilder() {
 			
 			@Override
@@ -91,14 +41,13 @@ public class CamelRoute {
 				
 				from("twitter://"
 						// query type = search
-//						+ "search?"
-						+ "streaming/filter?"
+						+ "search?"
+//						+ "streaming/filter?"
 						// query execution type = polling
 						+ "type=polling" + "&"
 						// poll every 10 sec
 						+ "delay=10" + "&"
-//						+ "keywords=" + searchCriteria + "&"
-						+ "locations=" + location + "&"
+						+ "keywords=" + searchCriteria + "&"
 						+ "consumerKey=HjLbxF1IoN1bXENLDuMPbJsQT" + "&"
 						+ "consumerSecret=62no3XxjQGrgWzASJXkyoW2L0Rs2Ba6Qi1OWFcwilgzOC1rWwW" + "&"
 						+ "accessToken=229302807-NwkYedrDGdZ4CDxvoRcxVtH8klPqfC9Yxt0fD9Fn" + "&"
@@ -108,33 +57,9 @@ public class CamelRoute {
 						 "\tTweeted at location: ${body.geoLocation}")
 						 .marshal().json(JsonLibrary.Jackson)
 						 .log("Marshalled to JSON:\n${body}")
-					.setHeader(Exchange.HTTP_METHOD, constant("POST"))
-					.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-					.to(DATA_STREAM_HOST
-							+ "data-stream-cache/"
-							+ "cache/"
-							+ "streaming-data/"
-							+ "new-eventt/"
-							+ location + "/")
 					 .log("==Route Completed==");
-//					.setBody(body().prepend(""
-//								+ "{"
-//								+ "\"username\":" + "\"" + username + "\", "
-//								+ "\"searchCriteria\":" + "\"" + searchCriteria + "\", "
-//								+ "\"tweet\":" + "\""
-//								+ ""))
-//					.setBody(body().append(""
-//							+ "\""
-//							+ "}"
-//							+ ""))
-//					.to(DATA_STREAM_HOST
-//							+ "data-stream-cache/"
-//							+ "cache/"
-//							+ "cache/"
-//							+ "");
 			}
 		});
-		}
 		
 		camelContext.start();
 	}
